92c16afbf2fda140b1c25bbc96dccdb9e402bf2a
luna <yljloveliucan@126.com>
2019-07-06 17:14:02 +0800

    项目结构由jar包变更为war包

--- src/main/java/com/gandoph/monitor_tuning/MonitorTuningApplication.java
+++ src/main/java/com/gandoph/monitor_tuning/MonitorTuningApplication.java
@@ -2,9 +2,10 @@ package com.gandoph.monitor_tuning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MonitorTuningApplication {
+public class MonitorTuningApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonitorTuningApplication.class, args);
